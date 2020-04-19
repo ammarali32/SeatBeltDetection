d01f9b0f8bd8813e70f5a00fd7fff5db98f1ebbe
vladislav <97b@mail.ru>
2020-04-19 17:09:17 +0300

    wip

--- BeltDetectionLab2/BeltDetectionLab2.py
+++ BeltDetectionLab2/BeltDetectionLab2.py
@@ -4,9 +4,10 @@ import numpy as np
 
 def build_filters():
     filters = []
-    ksize = 31
-    for theta in np.arange(0, np.pi, np.pi / 16):
-        kern = cv2.getGaborKernel((ksize, ksize), 0.3, theta, 9.0, 0.6, 50, ktype=cv2.CV_32F)
+    ksize = 9
+    theta = [cv2.getGaborKernel((ksize, ksize), 0.3, theta, 9.0, 0.6, 50, ktype=cv2.CV_32F)
+             for theta in np.arange(0, np.pi, np.pi / 16)]
+    kern = theta[-1]
     kern /= 1.5 * kern.sum()
     filters.append(kern)
     return filters
@@ -16,7 +17,7 @@ def process(img, filters):
     accum = np.zeros_like(img)
     for kern in filters:
         fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
-    np.maximum(accum, fimg, accum)
+        np.maximum(accum, fimg, accum)
     return accum
 
 
@@ -27,25 +28,32 @@ def main():
         layers_names = net.getLayerNames()
         outputlayers = [layers_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
         frame_id = 0
-
-        count = 0
+        image_counter = 0
 
         while True:
             _, frame = cap.read()
             frame_id += 1
-            cv2.fastNlMeansDenoising(frame, frame, 1, 7, 21)
             beltdetected = False
             height, width, channels = frame.shape
 
-            # Type you code here
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            clahe = cv2.createCLAHE(clipLimit=7.0, tileGridSize=(8, 8))
-            frame = clahe.apply(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-
+            first_clahe = cv2.createCLAHE(clipLimit=200.0, tileGridSize=(8, 8))
+            frame = first_clahe.apply(frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
             frame = cv2.convertScaleAbs(frame, alpha=1, beta=-40)
+            cv2.fastNlMeansDenoising(frame, frame, 1, 7, 21)
+
+            second_clahe = cv2.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
+            R, G, B = cv2.split(frame)
+
+            output1_R = second_clahe.apply(R)
+            output1_G = second_clahe.apply(G)
+            output1_B = second_clahe.apply(B)
+
+            frame = cv2.merge((output1_R, output1_G, output1_B))
 
             cv2.fastNlMeansDenoising(frame, frame, 3, 5, 11)
+
 
             filters = build_filters()
             frame = process(frame, filters)
@@ -59,7 +67,6 @@ def main():
                     scores = detection[5:]
                     class_id = np.argmax(scores)
                     confidence = scores[class_id]
-
                     if confidence > 0.2:
                         center_x = int(detection[0] * width)
                         center_y = int(detection[1] * height)
@@ -71,8 +78,8 @@ def main():
                         if class_id == 0:
                             beltdetected = True
 
-            print(count, ' ', beltdetected)
-            count += 1
+            print(image_counter, ' ', beltdetected)
+            image_counter += 1
             cv2.imshow("Image", frame)
             key = cv2.waitKey(1)
             if key == 27:
